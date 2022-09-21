8544bdd6faf33ce61a397c354b1ef48362c4b07e
b.inertia <bat.kittipat@gmail.com>
2022-09-21 18:06:54 +0700

    change ChangeCal code for more reader friendly

--- src/component/coinCalc.tsx
+++ src/component/coinCalc.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { CoinContainer, CoinValue } from "../styles";
 
 const CoinCalc = (Total: number) => {
@@ -28,82 +29,12 @@ const CoinCalc = (Total: number) => {
 
   return (
     <CoinContainer>
-      <CoinValue>
-        {array[0].x} :{array[0].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[1].x} :{array[1].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[2].x} :{array[2].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[3].x} :{array[3].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[4].x} :{array[4].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[5].x} :{array[5].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[6].x} :{array[6].y}$
-      </CoinValue>
-      <CoinValue>
-        {array[7].x} :{array[7].y}$
-      </CoinValue>
+      {array.map((a) => (
+        <CoinValue>
+          {a.x} : {a.y} $
+        </CoinValue>
+      ))}
     </CoinContainer>
   );
 };
 export default CoinCalc;
-
-// old draft
-// const CoinCalc = (x: number) => {
-//   let a = 0,
-//     b = 0,
-//     c = 0,
-//     d = 0,
-//     e = 0,
-//     f = 0,
-//     g = 0;
-//   x = x * 100;
-
-//   while (x - 1000 >= 0) {
-//     ++a;
-//     x -= 1000;
-//   }
-//   while (x - 500 >= 0) {
-//     ++b;
-//     x -= 500;
-//   }
-//   while (x - 200 >= 0) {
-//     ++c;
-//     x -= 200;
-//   }
-//   while (x - 150 >= 0) {
-//     ++d;
-//     x -= 150;
-//   }
-//   while (x - 20 >= 0) {
-//     ++e;
-//     x -= 20;
-//   }
-//   while (x - 10 >= 0) {
-//     ++f;
-//     x -= 10;
-//   }
-//   while (x - 5 >= 0) {
-//     ++g;
-//     x -= 5;
-//   }
-//   if (x > 4.9 && x < 5) {
-//     ++g;
-//   }
-//   while (g <= 2 && g - 2 >= 0) {
-//     g -= 2;
-//     ++f;
-//   }
-//   while (f <= 2 && f - 2 >= 0) {
-//     f -= 2;
-//     ++e;
-//   }
